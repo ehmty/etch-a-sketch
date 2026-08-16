361a688f1f65dfaa01778fc5d743a14712ee7377
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-16 21:59:47 +0200

    created createGrid function to size the grid according to the argument given, e.g. 16=> 16x16 squares

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+function createGrid(x) {
+    for (let col = 0; col < x*x; col++) {
+        const gridArea = document.querySelector("div");
+        gridArea.classList.add("grid")
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = 100 / x + "%";
+        square.style.height = 100 / x + "%";
+        gridArea.appendChild(square);
+    }
+}
+
+    
+createGrid(16);
