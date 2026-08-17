fa2838a0c057ebb6583049d6e0be8584734898a6
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-17 09:48:31 +0200

    added button functionality where user get prompted for the prefered grid size(x*x) and replaced previous grid

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const gridArea = document.querySelector("div");
+let gridArea = document.querySelector("div");
 const body = document.querySelector("body");
 
 function createGrid(x) {
+    let gridArea = document.querySelector("div");
+    if (!gridArea) {
+        gridArea = document.createElement("div");
+        gridArea.classList.add("grid");
+        body.appendChild(gridArea);
+    }
+
     for (let i = 0; i < x*x; i++) {
 
-        gridArea.classList.add("grid")
+        gridArea.classList.add("grid");
 
         const square = document.createElement("div");
         square.classList.add("square");
@@ -15,14 +22,24 @@ function createGrid(x) {
 
         square.addEventListener("mouseenter", () => square.style.backgroundColor = "black");
     }
+
+    let btn = document.querySelector("button");
+    if (!btn) {
+        btn = document.createElement("button");
+        btn.textContent = "new Grid";
+        btn.classList.add("btn");
+        body.insertBefore(btn, gridArea);
+    }
+    
+
+    btn.addEventListener("click", () => {
+        body.removeChild(gridArea);
+        const input = prompt("Please enter a number for the grid x*x");
+        createGrid(input);
+    })
+    
+
 }
 
-function createButton () {
-    const btn = document.createElement("button");
-    btn.textContent = "new Grid";
-    btn.classList.add("btn");
-    body.insertBefore(btn, gridArea);
-}
 
 createGrid(64);
-createButton();
